a3ed3fad38ae1599acd7f53925ea6e7cafd184ea
Eric Boszin <boszineric@gmail.com>
2023-02-09 22:06:11 -0500

    updates to webapp and frontend to trigger runs

--- sa-app/sa-frontend/src/App.js
+++ sa-app/sa-frontend/src/App.js
@@ -21,7 +21,7 @@ class App extends Component {
 
     analyzeSentence() {
         fetch('http://127.0.0.1:53419/sentiment', {
-        //fetch('<sa-webapp-lb-url>/sentiment', {
+        //fetch('<sa-webapp-lb-url-local>/sentiment', {
         //fetch(`${process.env.SA_WEBAPP_API_URL}/sentiment`, {
                 method: 'POST',
             headers: {
