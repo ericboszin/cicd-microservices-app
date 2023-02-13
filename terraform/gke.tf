c4b5109fb87c1feaf11effd0d0a4ffc3c9bafef2
Eric Boszin <boszineric@gmail.com>
2023-02-12 22:58:32 -0500

    triggering tfe workflow to deploy GKE

--- terraform/gke.tf
+++ terraform/gke.tf
@@ -78,4 +78,3 @@ resource "google_container_node_pool" "primary_nodes" {
 #   client_key             = google_container_cluster.primary.master_auth.0.client_key
 #   cluster_ca_certificate = google_container_cluster.primary.master_auth.0.cluster_ca_certificate
 # }
-
