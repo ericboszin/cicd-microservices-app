3fd7f736ea5f6bb750d1d4b54a843b026834d889
Eric Boszin <boszineric@gmail.com>
2023-02-13 17:52:27 -0500

    deploying resrouces

--- terraform/gke.tf
+++ terraform/gke.tf
@@ -27,7 +27,7 @@ resource "google_container_cluster" "primary" {
   network    = google_compute_network.vpc.name
   subnetwork = google_compute_subnetwork.subnet.name
 
-  # TODO: Enabling Autopilot for this cluster
+  # TODO: Enable Autopilot for this cluster
   # enable_autopilot = true
 }
 
